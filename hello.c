d264ffb5084eb2bf2dfd93f10b45bcc1de188c86
vipulg <vipugpta@gmail.com>
2021-07-05 23:19:09 +0530

    Adding c and cpp file

---
+++ hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+	printf ("hello from WSL\n");
+	return 0;
+}
---
+++ helloworld.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World from WSL" << endl;
+	return 0;
+}
